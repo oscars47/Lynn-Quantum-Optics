28a91817dc63a252dce8237ada6f2a555c4e7c79
Oscar Scholin <orsa2020@mymail.pomona.edu>
2023-03-05 02:46:52 -0800

    fixed soln

--- testing/nogo.py
+++ testing/nogo.py
@@ -2,17 +2,20 @@
 # @oscars47
 
 # using sympy to handle symbolic manipulation
-from sympy.solvers import solve
 from sympy import *
+# from sympy.solvers import solve, solveset
 from sympy.physics.quantum.dagger import Dagger
-from sympy.printing.mathematica import MCodePrinter
+# from sympy.printing.mathematica import MCodePrinter
 import numpy as np
 from itertools import combinations
+import pandas as pd
+import time
+import signal # limit time for solvers
 
 ## Initialize d value ##
 
 # d = int(input('Enter a value for d: '))
-d=2
+d=3
 
 ## Helper Functions for Bell State Generation ##
 
@@ -71,7 +74,7 @@ for i in range(2*d):
     alphabet.append(b_i)
     var_dict['v'+str(i)] = a_i+b_i*I
     # var_dict['v'+str(i)] = Symbol('v'+str(i))
-alphabet=tuple(alphabet)
+# alphabet=tuple(alphabet)
 # get normalization term: sum of modulus squared for each variable
 norm_ls = [Dagger(var_dict['v'+str(i)])*var_dict['v'+str(i)] for i in range(len(var_dict))]
 norm_ls.append(-1) # append a -1, i.e. sum_i v_iv_i^\dagger -1 = 0
@@ -120,7 +123,7 @@ for key in bs_dict:
 
 ## Choose k ##
 # k = int(input('Enter a value for k: '))
-k = 2
+k = 3
 print('k = ', k)
 
 ## Find all unique combinations of the d**2 BS choose k ##
@@ -130,6 +133,11 @@ k_groups = list(combinations(np.arange(0, len(measured_all)), k))
 
 ## Take inner product within all unique k groups ##
 # takes in k_group and exisiting eqn for the entire k group
+
+# define dataframe to log info about what possible pairs in k group generate solns
+# k is the k number, L is left qubit, R is right qubit (in the inner product), and num soln is the number of sets in the sol
+# global results
+results= pd.DataFrame({'d': [], 'k_group':[], 'num_soln':[]})
 def solve_k_eqn(k_group):
     print('-*----*---*-')
     print('using k group:', k_group)
@@ -142,87 +150,87 @@ def solve_k_eqn(k_group):
         inner_prod = Dagger(measured_all[pair[0]])*measured_all[pair[1]]
         # split result into real and imaginary components
         # print(inner_prod)
-        re_part= re(inner_prod)
-        im_part = im(inner_prod)
-        if re_part != Matrix([0]): # want only nonzero terms
+        re_part= re(inner_prod)[0]
+        im_part = im(inner_prod)[0]
+        if re_part != 0: # want only nonzero terms
             eqn_re.append(re_part)
             eqn_total.append(re_part)
-        if im_part != Matrix([0]):
+        if im_part != 0:
             eqn_im.append(im_part)
             eqn_total.append(im_part)
 
     eqn_re.append(re(norm_cond))
     eqn_total.append(re(norm_cond))
-    # eqn_im.append(im(norm_cond)) # no imaginary component of length
-    # eqn.append(im(norm_cond))
-    # eqs,S('(r5,r6,r9)'),manual=1,check=0,simplify=0
-    # print('len re:', len(eqn_re), 'len im:', len(eqn_im))
+    
     print('eqn re:', eqn_re)
     print('eqn im:', eqn_im) 
-    # soln = nsolve(eqn, [alphabet['a0'], alphabet['a1'], alphabet['a2'], alphabet['a3'], alphabet['b0'], alphabet['b1'], alphabet['b2'], alphabet['b3']], 0, dict=True)
-    # print(tuple(alphabet[:-1]))
-    # if len(eqn)<len(alphabet):
-    # #     soln = nsolve(tuple(eqn), tuple(alphabet[:-(len(alphabet) - len(eqn))]), tuple(np.zeros(len(alphabet)-1)), dict=True)
-    #     soln =solve(eqn, tuple(alphabet[:len(eqn)-len(alphabet)]), dict=True)
-    #     # print('here')
-    # else:
-    # #      soln = nsolve(tuple(eqn), tuple(alphabet), tuple(np.zeros(len(alphabet)-1)), dict=True)
-    #     soln =solve(eqn, tuple(alphabet), dict=True)
-    # sometimes im part or real part is 0
-    # if eqn_re!=[Matrix([[0]])]:
-    #     soln_re =solve(eqn_re,dict=True)
-    # else:
-    #     soln_re=[]
-    # if eqn_im!=[Matrix([[0]])]:
-    #     soln_im =solve(eqn_im,dict=True)
-    # else:
-    #     soln_im=[]
-
-    ## solving separately ##
-
-    # soln_re =solve(eqn_re,dict=True)
-    # soln_im =solve(eqn_im,dict=True) 
-
-    # if len(soln_re)>=1 and len(soln_im)>=1:
-    #     soln_re =solve(eqn_re,dict=True)[0]
-    #     soln_im =solve(eqn_im,dict=True)[0]
-    #     # combine eqns to solve for intersection
-    #     eqn_intersect = []
-    #     soln_re_dep = soln_re.keys() # dependent vars for real
-    #     for re_dep in soln_re_dep:
-    #         eqn_intersect.append(re_dep - soln_re[re_dep])
-    #     soln_im_dep = soln_im.keys() # dependent vars for real
-    #     for im_dep in soln_im_dep:
-    #         eqn_intersect.append(im_dep - soln_im[im_dep])
-
-    #     soln_intersect = solve(eqn_intersect)
-    #     print('soln of intersect:', soln_intersect)
-    #     print('len of soln intersect:', len(soln_intersect))
-    
-    # elif len(soln_re)>=1 and len(soln_im)<1:
-    #     soln_re =solve(eqn_re,dict=True)[0]
-    #     print('soln re (only):', soln_re)
-    #     print('len of re only:', len(soln_re))
-
-    # elif len(soln_im)>=1 and len(soln_re)<1:
-    #     soln_im =solve(eqn_im,dict=True)[0]
-    #     print('soln im (only):', soln_im)
-    #     print('len of im only:', len(soln_im))
-    
-    # eqn_total = []
-    # eqn_total.append(eqn_re)
-    # eqn_total.append(eqn_im)
     print('eqn total:', eqn_total)
-    soln = solve(eqn_total, alphabet, force=True, manual=True, dict=True)
+
+    
+    ## for timing out ##
+    class TimeoutException(Exception):   # Custom exception class
+        pass
+
+
+    def break_after(seconds=2):
+        def timeout_handler(signum, frame):   # Custom signal handler
+            raise TimeoutException
+        def function(function):
+            def wrapper(*args, **kwargs):
+                signal.signal(signal.SIGALRM, timeout_handler)
+                signal.alarm(seconds)
+                try:
+                    res = function(*args, **kwargs)
+                    signal.alarm(0)      # Clear alarm
+                    return res
+                except TimeoutException:
+                    print('Solve time exceeded :(')
+                return
+            return wrapper
+        return function
+
     # soln = solve(eqn_total, alphabet, force=True, dict=True)
-    print('soln:', soln)
+    global findsoln
+    global results_new
+    results_new=0
+    findsoln=False
+    
+    @break_after(10) # 30 second limit
+    def try_soln():  
+        global findsoln
+        global results_new
+        try:
+            soln = nonlinsolve(eqn_total, alphabet)
+            print('soln:', soln)
+            print(len(soln))
+            if len(soln)>0:
+                results_new = {'d': d, 'k_group':k_group, 'num_soln':len(soln)}
+                findsoln = True # found a solution!
+        except:
+            print('there was a problem with the solve')
+            # ValueError: Absolute values cannot be inverted in the complex domain.
+        
+
+    
+    try_soln()
+    if findsoln== True: # did find >= 1 solution:
+        print('found a solution for k=%i, exiting search.'%k)
+    return findsoln, results_new
     
     
 
     ## ISSUE: not finding symbolic solution ##
     # https://stackoverflow.com/questions/61548790/sympy-solve-returns-an-empty-set
-    
+
+# log computation time
+start_time = time.time()
 for k_group in k_groups:
-    solve_k_eqn(k_group)
-## Solve resultant system ##
-# need to implement check for time?
+    findsoln, results_new = solve_k_eqn(k_group)
+    if not(isinstance(results_new, int)):
+        results = results.append(results_new, ignore_index=True)
+    if findsoln==True:
+        break
+end_time = time.time()
+print(results)
+results.to_csv('d=%i,k=%i_results.csv'%(d, k))
+print('computation took %s seconds'%(end_time-start_time))
